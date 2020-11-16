16f4c8400acea653bd9a70c3483b3efab048f07a
shofujimoto0617 <shofujimoto017@gmail.com>
2020-11-16 23:24:50 +0900

    JAVA ４章　終了 確認問題　終了

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int RUCKY_NUMBER = 7;
+
+		String str1;
+		str1 = "FUJIMOTO";
+		System.out.println(str1);
+
+		str1 = "藤本";
+		System.out.println(str1);
+
+		System.out.println(RUCKY_NUMBER);
+	}
+
+}
