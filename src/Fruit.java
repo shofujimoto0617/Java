2a31dce6d78bba354ee3b0ce49b42a6845251928
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-20 19:02:57 +0900

    JAVA 応用編 4章

---
+++ src/Fruit.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+
+abstract class Fruit {
+	String name;
+	BigDecimal price;
+
+	Fruit(String name, BigDecimal price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	abstract protected String getName();
+	abstract protected BigDecimal getPrice();
+
+}
---
+++ src/Peach.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+
+class Peach extends Fruit {
+	Peach(String name, BigDecimal price) {
+		super(name, price);
+	}
+
+	@Override
+	protected String getName() {
+		return name;
+	}
+
+	@Override
+	protected BigDecimal getPrice() {
+		return price;
+	}
+
+}
